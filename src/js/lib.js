9e775aa4179e33045e74fb1ae4c3aeaf9a32bde7
Timoxa <andrei.kurlyandchik@gmail.com>
2017-12-11 20:35:51 +0300

    Add files via upload

---
+++ src/js/lib.js
@@ -0,0 +1,41 @@
+//=include lib/jquery.js
+
+//=include lib/jquery-ui.min.js
+
+//=include lib/datepicker-ru.js
+
+//=include lib/barba.js
+
+//=include lib/jquery.data-parallax.min.js
+//=include lib/jquery.autosize.js
+
+//=include lib/TweenLite.min.js
+//=include lib/CSSPlugin.min.js
+
+//=include lib/stickyfill.min.js
+
+//=include lib/fm.revealator.jquery.js
+
+//=include lib/swiper.min.js
+
+//=include lib/masonry.pkgd.min.js
+
+//=include lib/jquery.fancybox.min.js
+
+//=include lib/slick.js
+
+//=include lib/select2.min.js
+
+//=include lib/jquery.mCustomScrollbar.js
+
+//=include lib/jquery.waypoints.min.js
+
+//=include lib/jquery.countup.min.js
+
+//=include lib/jquery.form-validator.min.js
+
+//=include lib/sticky-kit.min.js
+
+//=include lib/parallax.min.js
+
+
